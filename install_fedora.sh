7808e60d6d6a09d435ba0985c7e8ac40ebcd4dd2
Thiag <109832582+ThiagSilvaDev@users.noreply.github.com>
2025-08-22 01:31:46 -0300

    Added script for easy setup

---
+++ install_fedora.sh
@@ -0,0 +1,113 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+FONT_DIR="$HOME/.local/share/fonts"
+
+PACKAGES=(
+    "curl"
+    "git"
+    "wget"
+    "neovim"
+    "zsh"
+    "tmux"
+    "btop"
+    "stow"
+    "fzf"
+    "ripgrep"
+    "fastfetch"
+    "foot"
+    "code"
+)
+
+install_packages() {
+    local packages=("$@")
+    log "Instalando pacotes: ${packages[*]}"
+
+    for package in "${packages[@]}"; do
+        if sudo dnf install -y "$package"; then
+            log "✓ $package instalado com sucesso"
+        else
+            warn "✗ Falha ao instalar $package"
+        fi
+    done
+}
+
+install_packages "${PACKAGES[@]}"
+
+FLATPAK_PACKAGES=(
+    "com.discordapp.Discord"
+    "com.usebruno.Bruno"
+    "md.obsidian.Obsidian"
+)
+
+install_flatpaks() {
+    local packages=("$@")
+    log "Instalando Flatpaks: ${packages[*]}"
+
+    for package in "${packages[@]}"; do
+        if flatpak install -y flathub "$package"; then
+            log "✓ $package instalado com sucesso"
+        else
+            warn "✗ Falha ao instalar $package"
+        fi
+    done
+}
+
+install_flatpaks "${FLATPAK_PACKAGES[@]}"
+
+
+# Docker install
+log "Docker install"
+
+sudo dnf -y install dnf-plugins-core
+sudo dnf-3 config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+
+DOCKER_PACKAGES=(
+    "docker-ce"
+    "docker-ce-cli"
+    "containerd.io"
+    "docker-buildx-plugin"
+    "docker-compose-plugin"
+)
+
+install_packages "${DOCKER_PACKAGES[@]}"
+
+sudo systemctl enable --now docker
+
+# ZSH, Oh My Zshell
+chsh -s "${which zsh}"
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+
+# Plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Theme
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+
+# VS Code
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+
+dnf check-update
+
+# Lazy Vim
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+
+
+# JetBrains font
+wget -O /tmp/JetBrainsMono.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip
+unzip /tmp/JetBrainsMono.zip -d "$FONT_DIR/JetBrainsMono/"
+
+stow .
+
+
+
+
+log "Limpando cache"
+sudo dnf autoremove -y
+sudo dnf clean all
+
+echo
+log "Configuração concluída"
